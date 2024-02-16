fa9311c3fe4566f71449a97859617843225d7b8b
mohamedali3521 <mohamedali321k@gmail.com>
2024-02-16 22:53:02 +0530

    initial setup

---
+++ HVPO/src/test/java/com/HVPO/HVPO/HvpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HVPO.HVPO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
